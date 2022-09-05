8f99365c7572c28f9881aa3f801bce2f76b14519
zach1zou <791522322@qq.com>
2022-09-05 11:47:44 +0800

    提交

---
+++ pages/cart/cart.vue
@@ -0,0 +1,19 @@
+<template>
+  <view>
+    2
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="less">
+
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <view>
+    <view class="topSearch">
+      <view class="search_btn">
+        搜索
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="less">
+.topSearch {
+  width: 750rpx;
+  height: 100px;
+  background-color: #c00000;
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  .search_btn {
+    width: 400rpx;
+    height: 30px;
+    margin-top: 50px;
+    background-color: #fff;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
